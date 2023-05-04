d8164b5f6fbd3bec6a64d2dc8bcf6a07dd915855
Zedek <mob@jetstreamafrica.com>
2023-05-04 01:19:53 +0000

    removed unneeded route

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,8 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Jetstream\Curacel\Http\Controllers\CustomerController;
 
-Route::get('test',[CustomerController::class,'index']);
-
 
 Route::prefix('customer')->group(function (){
     Route::get('',[CustomerController::class,'index']);
