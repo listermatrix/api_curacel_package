e74d5aea9b467373f946d3c5ed565ac0b2e47077
Zedek <mob@jetstreamafrica.com>
2023-05-11 01:25:24 +0000

    updated feature with claims

--- src/API/Interface/IClaimService.php
+++ src/API/Interface/IClaimService.php
@@ -4,5 +4,8 @@ namespace Jetstream\Curacel\API\Interface;
 
 interface IClaimService
 {
-
+    public function createClaim();
+    public function getAllClaims();
+    public function getClaim();
+    public function updateDischargeVoucher();
 }
